c7c56b27f06439550a5028fe06bc27e0d668ab5e
Shankar <jarvisbyshankar@gmail.com>
2024-04-22 12:54:06 +0800

    SGD classifier for 5 and non-5

--- ML-Samples/digit-recogniser/digit-recogniser.py
+++ ML-Samples/digit-recogniser/digit-recogniser.py
@@ -1,5 +1,4 @@
-# Following implementation is based on the book Hands-On Machine Learning with Scikit-Learn, Keras, and TensorFlow 3rd Edition
-# by Aurélien Géron
+# Following implementation is based on the book Hands-On Machine Learning with Scikit-Learn, Keras, and TensorFlow 3rd Edition by Aurélien Géron
 
 from sklearn.datasets import fetch_openml
 
@@ -19,3 +18,19 @@ def plot_digit(image_data):
 test_digit = X[0]
 plot_digit(test_digit)
 plt.show()
+
+# MNIST dataset is already split into training set (first 60000) and test set (last 10000), with good shuffled test set
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+# let us first do a binary classification like distinguish 5 and non-5
+
+# set True to all 5s and False to others
+y_train_5 = (y_train == '5')
+y_test_5 = (y_train == '5')
+
+from sklearn.linear_model import SGDClassifier
+
+sgd_classifier = SGDClassifier(random_state=42)
+sgd_classifier.fit(X_train, y_train_5)
+
+print(sgd_classifier.predict([test_digit]))
